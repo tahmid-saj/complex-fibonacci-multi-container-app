f801045f45dcf89d1d47b29631469ac4ee72ca99
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-06 21:45:24 -0400

    Express api

--- server/index.js
+++ server/index.js
@@ -25,3 +25,50 @@ pgClient.on("connect", (client) => {
     .catch((err) => console.error(err));
 });
 
+// Redis client setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("select * from values");
+
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.value;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  redisClient.hset("values", index, "Nothing yet");
+  redisPublisher.publish("insert", index);
+  pgClient.query("insert into values(number) values($1)", [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log("Listening on port 5000");
+});
+
+
