0462ba93eb12f34a7fea25eace3db7542c049e11
Tahmid Sajin <tahmid.saj@gmail.com>
2023-08-06 21:01:46 -0400

    Worker process

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function fib(index) {
 
   return fib(index - 1) + fib(index - 2);
 }
+
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib(parseInt(message)))
+});
+sub.subscribe("insert");
+
